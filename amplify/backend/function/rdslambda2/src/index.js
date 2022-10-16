dae0ed13b753930ff40ab8cef0d010c1d1aa713c
Koji-Sunioj <koji.gabriel218@gmail.com>
2022-10-16 15:36:16 +0300

    refactor

--- amplify/backend/function/rdslambda2/src/index.js
+++ amplify/backend/function/rdslambda2/src/index.js
@@ -1,9 +1,9 @@
 const { Pool } = require("pg");
-const { successObject, rejectObject } = require("./utils/headers");
-const { verifyToken } = require("./utils/token");
 const { getSecret } = require("./utils/ssm");
+const { verifyToken } = require("./utils/token");
 const { checkSameUser } = require("./utils/check");
 const { depositS3, removeS3 } = require("./utils/s3");
+const { successObject, rejectObject } = require("./utils/headers");
 
 exports.handler = async (event) => {
   const { user, host, database, password, port, bucket } = await getSecret(
@@ -19,14 +19,20 @@ exports.handler = async (event) => {
 
   const { httpMethod, resource, pathParameters, headers } = event;
   const routeKey = `${httpMethod} ${resource}`;
-  let query, command, values, isValidUser;
+  let query,
+    command,
+    values,
+    isValidUser,
+    requester,
+    user_email,
+    type,
+    isSameUser;
 
   switch (routeKey) {
     case "GET /complaints":
       query = await pool.query(
         "select id,complaint,user_email,picture from complaints order by id ASC;"
       );
-      await pool.end();
       return {
         ...successObject,
         body: JSON.stringify(query.rows),
@@ -41,14 +47,12 @@ exports.handler = async (event) => {
       };
 
     case "POST /complaints":
-      isValidUser = await verifyToken(headers);
-      switch (isValidUser.type) {
-        case "guest":
-          return rejectObject;
+      ({ user_email, type } = await verifyToken(headers));
+      switch (type) {
         case "user":
           const client = await pool.connect();
           const { complaint, file } = JSON.parse(event.body);
-          values = [complaint, isValidUser.user_email];
+          values = [complaint, user_email];
           await client.query("BEGIN");
           command =
             "INSERT INTO complaints(complaint,user_email) VALUES($1,$2) RETURNING id;";
@@ -71,88 +75,74 @@ exports.handler = async (event) => {
               ...query.rows[0],
             }),
           };
+        default:
+          return rejectObject;
       }
     case "DELETE /complaints/{id}":
-      isValidUser = await verifyToken(headers);
-      switch (isValidUser.type) {
-        case "guest":
-          return rejectObject;
-        case "user":
-          const isSameUser = await checkSameUser(
-            pool,
-            isValidUser.user_email,
-            pathParameters.id
+      ({ user_email, type } = await verifyToken(headers));
+      isSameUser = await checkSameUser(pool, user_email, pathParameters.id);
+      requester = `${type} ${isSameUser}`;
+      switch (requester) {
+        case "user same":
+          query = await pool.query(
+            `delete from complaints where id=${pathParameters.id} returning picture;`
           );
-
-          if (isSameUser) {
-            query = await pool.query(
-              `delete from complaints where id=${pathParameters.id} returning picture;`
-            );
-            const { picture } = query.rows[0];
-            if (picture) {
-              const complaintPic = picture.match(/complaint_[0-9]{1,3}/g)[0];
-              await removeS3(complaintPic, bucket);
-            }
-            return {
-              ...successObject,
-              body: JSON.stringify({
-                message: `complaint ${pathParameters.id} deleted`,
-              }),
-            };
-          } else {
-            return rejectObject;
+          const { picture } = query.rows[0];
+          if (picture) {
+            const complaintPic = picture.match(/complaint_[0-9]{1,3}/g)[0];
+            await removeS3(complaintPic, bucket);
           }
+          return {
+            ...successObject,
+            body: JSON.stringify({
+              message: `complaint ${pathParameters.id} deleted`,
+            }),
+          };
+        default:
+          return rejectObject;
       }
     case "PATCH /complaints/{id}":
-      isValidUser = await verifyToken(headers);
-      switch (isValidUser.type) {
-        case "guest":
-          return rejectObject;
-
-        case "user":
-          const isSameUser = await checkSameUser(
-            pool,
-            isValidUser.user_email,
-            pathParameters.id
-          );
-
-          if (isSameUser) {
-            const client = await pool.connect();
-            const { complaint, file, removePhoto } = JSON.parse(event.body);
-            await client.query("BEGIN");
-            command =
-              "UPDATE complaints set complaint=$1 where id=$2 RETURNING *;";
-            query = await client.query(command, [complaint, pathParameters.id]);
-            if (removePhoto) {
-              await removeS3(removePhoto, bucket);
-              command = "UPDATE complaints set picture = null where id = $1;";
-              await client.query(command, [pathParameters.id]);
-            } else if (file) {
-              const uri = await depositS3(
-                {
-                  ...file,
-                  name: `complaint_${pathParameters.id}`,
-                },
-                bucket
-              );
-              const { picture } = query.rows[0];
-              if (picture === null) {
-                command = "UPDATE complaints set picture = $1 where id = $2;";
-                await client.query(command, [uri, pathParameters.id]);
-                query.rows[0].picture = uri;
-              }
+      ({ user_email, type } = await verifyToken(headers));
+      isSameUser = await checkSameUser(pool, user_email, pathParameters.id);
+      requester = `${type} ${isSameUser}`;
+      switch (requester) {
+        case "user same":
+          const client = await pool.connect();
+          const { complaint, file, removePhoto } = JSON.parse(event.body);
+          await client.query("BEGIN");
+          command =
+            "UPDATE complaints set complaint=$1 where id=$2 RETURNING *;";
+          query = await client.query(command, [complaint, pathParameters.id]);
+          if (removePhoto) {
+            await removeS3(removePhoto, bucket);
+            command = "UPDATE complaints set picture = null where id = $1;";
+            await client.query(command, [pathParameters.id]);
+          } else if (file) {
+            const uri = await depositS3(
+              {
+                ...file,
+                name: `complaint_${pathParameters.id}`,
+              },
+              bucket
+            );
+            const { picture } = query.rows[0];
+            if (picture === null) {
+              command = "UPDATE complaints set picture = $1 where id = $2;";
+              await client.query(command, [uri, pathParameters.id]);
+              query.rows[0].picture = uri;
             }
-            await client.query("COMMIT");
-            return {
-              ...successObject,
-              body: JSON.stringify({
-                message: `complaint ${pathParameters.id} updated`,
-                ...query.rows[0],
-              }),
-            };
-          } else {
-            return rejectObject;
           }
+          await client.query("COMMIT");
+          return {
+            ...successObject,
+            body: JSON.stringify({
+              message: `complaint ${pathParameters.id} updated`,
+              ...query.rows[0],
+            }),
+          };
+
+        default:
+          return rejectObject;
       }
     default:
       return {
--- amplify/backend/function/rdslambda2/src/utils/check.js
+++ amplify/backend/function/rdslambda2/src/utils/check.js
@@ -1,15 +1,13 @@
-const { Pool } = require("pg");
-
 const checkSameUser = async (pool, user_email, id) => {
   const checkCommand =
     "select \
-        case when user_email = $1 then 1 else 0 end as same_user\
+        case when user_email = $1 then 'same' else 'different' end as same_user\
         from complaints\
         where id = $2;";
   const values = [user_email, id];
   const query = await pool.query(checkCommand, values);
-  const isSameUser = Boolean(query.rows[0].same_user);
-  return isSameUser;
+  const { same_user } = query.rows[0];
+  return same_user;
 };
 
 module.exports = { checkSameUser };
